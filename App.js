6197c01eaf6212a8ac7156b184f82f21d4a46880
Myungkyum Kim <myungyum.kim@sunnyfive.net>
2020-09-14 20:54:20 +0900

    [All] clone fokin
    fokin weather has been finished finally
    
    Signed-off-by: Myungkyum Kim <myungyum.kim@sunnyfive.net>

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Alert } from "react-native";
+import Loading from "./Loading.js";
+import * as Location from "expo-location";
+import axios from "axios";
+import Weather from "./Weather.js";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const API_KEY = "035918c0d1bbb563b73bfd99a64176b5";
+
+export default class App extends React.Component {
+  state = {
+    isLoading: true,
+  };
+  getWeather = async (latitude, longitude) => {
+    const {
+      data: {
+        main: { temp },
+        weather
+      },
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+    this.setState({
+      isLoading: false,
+      condition: weather[0].main,
+      temp: temp
+    });
+    console.log("fetched weather", temp, weather[0].main);
+    console.log("fetched state", this.state);
+  };
+  getLocation = async () => {
+    try {
+      //Alert.alert("Hello", "gogo");
+      const response = await Location.requestPermissionsAsync();
+      console.log(response);
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+      console.log(latitude, longitude);
+    } catch (error) {
+      Alert.alert("Can't find you", "So sad");
+    }
+  };
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather temp={temp} condition={condition} />
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Web
+# Fonkin Weather
